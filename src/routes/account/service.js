23433bcc81d733db58746e7929cdabf195478c72
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-03-31 21:15:48 +0300

    Improve Account route editAccount method

--- src/routes/account/service.js
+++ src/routes/account/service.js
@@ -1,5 +1,7 @@
 import joi from 'joi';
-import { userError, getResponse } from '@helpers';
+import { userError, getResponse, validateAccessToken } from '@helpers';
+import { apiErrors } from '@constants';
+import { userModel } from '@models';
 
 const contactUs = async (req) => {
   const body = req.body;
@@ -23,9 +25,34 @@ const getAccount = async (req) => {
 };
 
 const editAccount = async (req) => {
-  const body = req.body;
+  try {
+    const body = req.body;
+    const headers = req.headers;
 
-  return getResponse(body);
+    const editAccountSchema = joi.object({
+      username: joi.string().required(),
+      name: joi.string(),
+      surname: joi.string(),
+      age: joi.number(),
+      job: joi.string()
+    });
+
+    const validatedBody = await editAccountSchema.validateAsync(body);
+
+    const accessToken = headers['authorization'].replace('Bearer ', '');
+
+    const jwtAccessData = validateAccessToken(accessToken);
+
+    const user = await userModel.findByIdAndUpdate(jwtAccessData.id, validatedBody, { new: true }).exec();
+
+    if (!user) {
+      throw userError(apiErrors.user.notFound, 404);
+    }
+
+    return getResponse(user.getPublicFields());
+  } catch (err) {
+    throw userError(err.message, 400);
+  }
 };
 
 const changePassword = async (req) => {
