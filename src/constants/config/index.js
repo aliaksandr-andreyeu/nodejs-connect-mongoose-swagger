1e4f7a4ad5ad178f9c8a41741da0623203c4cd95
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2023-05-27 08:52:22 +0300

    Setup number env vars

--- src/constants/config/index.js
+++ src/constants/config/index.js
@@ -10,17 +10,17 @@ dotenv.config({
 
 const config = {
   host: env.HOST || 'localhost',
-  port: env.PORT || 3000,
+  port: Number(env.PORT) || 3000,
   dbHost: env.DB_HOST || 'localhost',
-  dbPort: env.DB_PORT || 27017,
+  dbPort: Number(env.DB_PORT) || 27017,
   dbName: env.DB_NAME || '',
   dbUser: env.DB_USER || '',
   dbPass: env.DB_PSW || '',
   accessTokenKey: env.ACCESS_TOKEN_KEY || '',
-  accessTokenExpiresIn: env.ACCESS_TOKEN_EXPIRES_IN || 5,
+  accessTokenExpiresIn: Number(env.ACCESS_TOKEN_EXPIRES_IN) || 5,
   refreshTokenKey: env.REFRESH_TOKEN_KEY || '',
-  refreshTokenExpiresIn: env.REFRESH_TOKEN_EXPIRES_IN || 10,
-  responseTimeout: env.RESPONSE_DELAY || 0
+  refreshTokenExpiresIn: Number(env.REFRESH_TOKEN_EXPIRES_IN) || 60,
+  responseTimeout: Number(env.RESPONSE_DELAY) || 0
 };
 
 export default config;
